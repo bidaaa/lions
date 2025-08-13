937ee470e67dc040deb5ce1ad0dd5660c5fc1c15
bida <bidaviniciusribas@gmail.com>
2025-08-13 09:38:05 -0300

    criado o menu

---
+++ unidade2/modulo1/crudusuarios/app.js
@@ -0,0 +1,36 @@
+import PromptSync from "prompt-sync";
+export const prompt = PromptSync({ sigint: true });
+import { adicionarUsuario } from "./adicionar.js";
+
+
+export let usuarios = []
+
+export function exibirMenu(){
+    console.clear()
+    console.log("----CADASTRO----")
+    console.log("Selecione a opção desejada:\n1-Adicionar usuário\n2-Listar usuários\n3-Atualizar usuário\n4-Remover usuário\n0-Sair\n")
+    let opcao = prompt("> ")
+    opcao = parseInt(opcao)
+    switch(opcao){
+        case 1:
+            adicionarUsuario()
+            break
+        case 2:
+            listarUsuario()
+            break
+        case 3:
+            editarUsuario()
+            break
+        case 4:
+            removerUsuario()
+            break
+        case 0:
+            process.exit()
+        default:
+            console.log("Opção inválida, tente novamente.")
+            exibirMenu()
+            break
+    }
+}
+
+exibirMenu()
