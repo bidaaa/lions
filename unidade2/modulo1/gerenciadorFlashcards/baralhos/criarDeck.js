46e896c78cae41acc7076389dbe492ac45bca7cf
bida <bidaviniciusribas@gmail.com>
2025-08-18 11:58:37 -0300

    criar baralho e criar flashcard

--- unidade2/modulo1/gerenciadorFlashcards/baralhos/criarDeck.js
+++ unidade2/modulo1/gerenciadorFlashcards/baralhos/criarDeck.js
@@ -1,5 +1,27 @@
 import { prompt, exibirMenu } from "../app.js";
 
+let quantBaralho = 0
+
 export function criarBaralho(){
-    
+    console.clear()
+    console.log("Qual o nome do baralho?")
+    let nome = prompt("> ")
+    nome = nome.toLowerCase()
+    quantBaralho++
+    const baralho = {
+        nome: nome,
+        id: quantBaralho
+    }
+
+    console.log("Baralho criado com sucesso!")
+    console.log("Deseja criar mais algum baralho?\n1- Sim\nOutro- Sair")
+    let opcao = prompt("")
+    switch(opcao){
+        case "1":
+            criarBaralho()
+            break
+        default:
+            exibirMenu()
+            break
+    }
 }
--- unidade2/modulo1/gerenciadorFlashcards/flashcards/criarFC.js
+++ unidade2/modulo1/gerenciadorFlashcards/flashcards/criarFC.js
@@ -1,5 +1,30 @@
 import { prompt, exibirMenu } from "../app.js";
 
+let quantFC = 0
+
 export function criarFlashcard(){
-    
+    console.clear()
+    console.log("Qual a pergunta do flashcard?")
+    let pergunta = prompt("> ")
+    console.log("Qual a resposta?")
+    let resposta = prompt("> ")
+    quantFC++
+
+    const flashcard = {
+        pergunta: pergunta,
+        resposta: resposta,
+        id: quantFC
+    }
+
+    console.log("Flashcard criado com sucesso!")
+    console.log("Deseja criar mais algum flashcard?\n1- Sim\nOutro- Sair")
+    let opcao = prompt("")
+    switch(opcao){
+        case "1":
+            criarFlashcard()
+            break
+        default:
+            exibirMenu()
+            break
+    }
 }
