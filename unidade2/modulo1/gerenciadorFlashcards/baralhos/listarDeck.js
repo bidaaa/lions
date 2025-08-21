ddbd72d6fe8db0de85e4b0a0ae54123cec122a09
bida <bidaviniciusribas@gmail.com>
2025-08-21 09:01:35 -0300

    funções listarFlashcards e listarBaralhos funcionando!

--- unidade2/modulo1/gerenciadorFlashcards/baralhos/listarDeck.js
+++ unidade2/modulo1/gerenciadorFlashcards/baralhos/listarDeck.js
@@ -1,5 +1,15 @@
-import { prompt, exibirMenu } from "../app.js";
+import { prompt, exibirMenu, baralhos } from "../app.js";
 
-export function listarBaralhos(){
-    
+export function listarBaralhos() {
+  console.clear();
+  baralhos.forEach((baralho) => {
+    console.log(`Nome: ${baralho.nome} || ID: ${baralho.id}`);
+  });
+  console.log("\nPressione enter para voltar ao menu.");
+  let opcao = prompt("");
+  switch (opcao) {
+    default:
+      exibirMenu();
+      break;
+  }
 }
--- unidade2/modulo1/gerenciadorFlashcards/flashcards/listarFC.js
+++ unidade2/modulo1/gerenciadorFlashcards/flashcards/listarFC.js
@@ -1,5 +1,15 @@
-import { prompt, exibirMenu } from "../app.js";
+import { prompt, exibirMenu, flashcards } from "../app.js";
 
-export function listarFlashcards(){
-    
+export function listarFlashcards() {
+  console.clear();
+  flashcards.forEach((flashcard) => {
+    console.log(`Pergunta: ${flashcard.pergunta} || Resposta: ${flashcard.resposta} || ID: ${flashcard.idFlashcard} || ID do baralho ao qual ele pertence: ${flashcard.idBaralho}`);
+  });
+  console.log("\nPressione enter para voltar ao menu.");
+  let opcao = prompt("");
+  switch (opcao) {
+    default:
+      exibirMenu();
+      break;
+  }
 }
