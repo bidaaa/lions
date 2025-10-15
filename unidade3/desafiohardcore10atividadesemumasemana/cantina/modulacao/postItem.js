5bf9e327fd2d1b619790718f970b2eed0a13a310
bida <bidaviniciusribas@gmail.com>
2025-10-15 11:44:06 -0300

    Função post adicionada!

--- unidade3/desafiohardcore10atividadesemumasemana/cantina/modulacao/postItem.js
+++ unidade3/desafiohardcore10atividadesemumasemana/cantina/modulacao/postItem.js
@@ -1,2 +1,18 @@
 import { Item } from "../item_schema.js";
 
+const createItem = async (nome, preco, diaSemana) => {
+    try{
+        const newItem = new Item({nome, preco, diaSemana})
+        return await newItem.save() 
+    }catch(error){
+        console.error("Erro ao criar item", error.message)
+        throw error
+    }
+}
+
+export async function postItem(req,res){
+    const {nome, preco, diaSemana} = req.body
+    const newItem = await createItem(nome, preco, diaSemana)
+
+    res.status(201).send({message: "Item criado com sucesso", item: newItem})
+}
